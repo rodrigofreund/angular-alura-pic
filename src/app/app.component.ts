affbd2f96be064e2d02495e9514710af52a65039
Rodrigo Freund <rodrigofreund@gmail.com>
2022-09-10 18:14:47 -0300

    criando modulo e exportando seu componente interno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alura-pic';
+
+  photos = [
+    {
+      url:'https://www.infoescola.com/wp-content/uploads/2010/06/tigre-de-bengala-60322900.jpg',
+      description:'tigre',
+    },
+    {
+      url:'https://static.todamateria.com.br/upload/le/ao/leaojuba-cke.jpg',
+      description:'leao',
+    },
+    {
+      url:'https://www.infoescola.com/wp-content/uploads/2010/06/tigre-de-bengala-60322900.jpg',
+      description:'tigre',
+    },
+    {
+      url:'https://static.todamateria.com.br/upload/le/ao/leaojuba-cke.jpg',
+      description:'leao',
+    }
+  ]
 }
